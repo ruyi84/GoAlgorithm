2ef5890dee5baa0eaddf8152adfca350f9142230
ruyi84 <ruyi_Ican@163.com>
2019-06-25 16:06:47 +0800

    T1's new solution

--- src/Topic/T1.go
+++ src/Topic/T1.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 func twoSum(nums []int, target int) []int {
 	i := make([]int, 2, 2)
 	for k1, v1 := range nums {
